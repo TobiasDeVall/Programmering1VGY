5a6b2f228756ec93ad225ca23c180138702b2e6b
TobiasDeVall <tobias.de.vall@gmail.com>
2017-03-10 12:16:03 +0100

    update nr7

---
+++ While_Satser/While_Satser/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Satser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning 1.1
+            /*
+            int i = 0;
+            while (i++ < 100)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            //Övning 1.2
+            /*
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+
+            while (password != "Tobbetheman")
+            {
+                Console.WriteLine("Wrong Password!");
+                password = Console.ReadLine();
+
+            }
+            Console.WriteLine("Welcome!");
+            */
+
+
+            //Övning 2.1
+            /*
+            int i = 0;
+
+            while (i++ < 100)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            //Övning 2.2
+            /*
+            int i = 11;
+
+            while (i-- > 1)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            //Övning 2.3
+            /*
+            while (true)
+            {
+                Console.WriteLine("Tobbe is the best!");
+            }
+            */
+
+            //Övning 2.4
+            /*
+            Random random = new Random();
+            int tal_n = random.Next(0, 101 );
+            int gissningar = 0;
+
+            Console.WriteLine("Gissa ett tal mellan 1 och 100: ");
+            string text = Console.ReadLine();
+            int tal = int.Parse(text);
+
+            while (tal != tal_n)
+            {
+                if (tal > tal_n)
+                {
+                    gissningar++;
+                    Console.WriteLine("För Högt!");
+                    Console.WriteLine("Gissa ett tal mellan 1 och 100: ");
+                    text = Console.ReadLine();
+                    tal = int.Parse(text);
+                }
+                else if (tal < tal_n)
+                {
+                    gissningar++;
+                    Console.WriteLine("För Lågt!");
+                    Console.WriteLine("Gissa ett tal mellan 1 och 100: ");
+                    text = Console.ReadLine();
+                    tal = int.Parse(text);
+                } 
+            }
+            Console.WriteLine("Grattis du gissade rätt!");
+            Console.WriteLine("Det tog dig {0} gissningar!", gissningar);
+            */
+
+            //Övning 2.5
+            int fib_1 = 0;
+            int fib_2 = 1;
+            int fib_n;
+
+
+            while (fib_1 < 1000)
+            {
+                fib_1 = fib_1 + fib_2;
+                Console.Write("1" + fib_1);
+
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Övning 6/Övning 6/Program.cs
@@ -0,0 +1,63 @@
+﻿/* 
+ * Skapad av Tobias de Vall och Elliot de Wrang
+ * 2017/01/27
+ * Programmet letar efter den pythagoreiska trippeln där a+b+c=1000
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Övning_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch stopwatch = Stopwatch.StartNew(); //creates and start the instance of Stopwatch
+                                                        //your sample code
+
+            double a;
+            double b;
+            double c;
+            double c2;
+            double total;
+
+            Random rnd = new Random();
+
+            for (int i = 0; true ; i++)
+            {
+                a = rnd.Next(300);
+                b = rnd.Next(400);
+
+                if(b > a)
+                {
+                    c2 = (a * a) + (b * b);
+
+                    c = Math.Sqrt(c2);
+
+                    total = a + b + c;
+
+                    if (c > b && total == 1000)
+                    {
+                        Console.WriteLine("a = " + a);
+                        Console.WriteLine("b = " + b);
+                        Console.WriteLine("c = " + c);
+                        Console.WriteLine("Total = " + total);
+
+                        stopwatch.Stop();
+                        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+                        break;
+                    }
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
