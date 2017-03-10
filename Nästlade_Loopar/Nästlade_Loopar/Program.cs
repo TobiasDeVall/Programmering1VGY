76db99d7b47246423f57d32d123da824c3e5bd4b
TobiasDeVall <tobias.de.vall@gmail.com>
2017-03-10 12:05:37 +0100

    update nr4

---
+++ Nästlade_Loopar/Nästlade_Loopar/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nästlade_Loopar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning 1
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    Console.Write("o#");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //Övning 2
+            /*
+            for (int i = 0; i < 11; i++)
+            {
+                int iModulo = i % 2;
+                if (iModulo == 0)
+                {
+                    for (int x = 0; x < 5; x++)
+                    {
+                        Console.Write("o#");
+                    }
+                    Console.Write("o");
+                }
+                else if(iModulo != 0)
+                {
+                    for (int y = 0; y < 5; y++)
+                    {
+                        Console.Write("#o");
+                    }
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //Övning 3
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                int antalBort = 10 - i;
+
+                for (int x = 0; x < antalBort; x++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //Övning 4
+            /*
+            for (int i = 0; i <= 10; i++)
+            {
+                int antalBort = 10 - i;
+
+                for (int x = 0; x < antalBort; x++)
+                {
+                    Console.Write("*");
+                }
+                
+                for (int y = 0; y < i; y++)
+                {
+                    Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //Övning 5
+            /*
+            for (int i = 0; i < 7; i++)
+            {
+                int teckenEtt = i;
+                int teckenTva = 6 - i;
+                for (int x = 0; x < 7; x++)
+                {
+                    if (x == i || x == teckenTva)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //Övning 6
+
+            for (int i = 0; i < 7; i++)
+            {
+                int teckenEtt = i;
+                int teckenTva = 6 - i;
+                for (int x = 0; x < 7; x++)
+                {
+                    if (x == i || x == teckenTva)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prov 1/Prov 1/Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * prov 1
+    tobias de vall 
+    25 November 2016
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uppgift 4
+            
+            double[] längdOchVikt = new double[10];
+            int räknare = 0;
+            double medelVikt = 0;
+            double medelLängd = 0;
+            
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Skriv in längd på person nr " + (i + 1));
+                längdOchVikt[räknare] = double.Parse(Console.ReadLine());
+                räknare = räknare + 1;
+                Console.WriteLine("Skriv in vikt på person nr " + (i + 1));
+                längdOchVikt[räknare] = double.Parse(Console.ReadLine());
+                räknare = räknare + 1;
+                
+           }
+     
+            
+                medelLängd = längdOchVikt[0] + längdOchVikt[2] + längdOchVikt[4] + längdOchVikt[6] + längdOchVikt[8];
+                medelVikt = längdOchVikt[1] + längdOchVikt[3] + längdOchVikt[5] + längdOchVikt[7] + längdOchVikt[9];
+
+            medelLängd = medelLängd / 5;
+            medelVikt = medelVikt / 5;
+
+            medelLängd = Math.Round(medelLängd, 2);
+            medelVikt = Math.Round(medelVikt, 2);
+
+            Console.WriteLine("Medelvikten: " + medelVikt + "kg");
+            Console.WriteLine("Medellängd: " + medelLängd + "cm");
+
+            double viktPerson1 = längdOchVikt[1];
+            double längdPerson1 = längdOchVikt[0];
+
+            Console.WriteLine("BMI värde person 1:");
+            BmiVärde(viktPerson1, längdPerson1);
+
+            Console.ReadKey();
+        }
+        //Uppgift 5
+        static string BmiVärde(double viktPerson1, double längdPerson1)
+        {
+            string bmiVärdePerson1Text = "";
+            double bmiVärdePerson1 = (längdPerson1*längdPerson1) / viktPerson1;
+
+            if (bmiVärdePerson1 < 18.5){
+                bmiVärdePerson1Text = "undervikt";
+            }
+            else if (bmiVärdePerson1 > 18.5 && bmiVärdePerson1 < 24.9)
+            {
+                bmiVärdePerson1Text = "normalvikt";
+            }
+            else if (bmiVärdePerson1 > 25 && bmiVärdePerson1 < 29.9)
+            {
+                bmiVärdePerson1Text = "Övervikt";
+            }
+            else if (bmiVärdePerson1 > 30 && bmiVärdePerson1 < 34.9)
+            {
+                bmiVärdePerson1Text = "Fetma grad 1";
+            }
+            else if (bmiVärdePerson1 > 35 && bmiVärdePerson1 < 39.9)
+            {
+                bmiVärdePerson1Text = "Fetma grad 2";
+            }
+            else if (bmiVärdePerson1 > 40)
+            {
+                bmiVärdePerson1Text = "Fetma grad 3";
+            }
+
+            return bmiVärdePerson1Text;
+        }
+    }
+}
